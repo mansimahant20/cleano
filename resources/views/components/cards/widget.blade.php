6786801aa9582157e299214b8976adf1870ab145
Mansi Mahant <mansi.ywppl@gmail.com>
2024-05-25 16:56:32 +0530

    Fixed: Advanced dashboard design

--- resources/views/components/cards/widget.blade.php
+++ resources/views/components/cards/widget.blade.php
@@ -1,10 +1,13 @@
 @php
     $bgClasses = ['widget-bg-1', 'widget-bg-2', 'widget-bg-3', 'widget-bg-4', 'widget-bg-5', 'widget-bg-6', 'widget-bg-7', 'widget-bg-8'];
-    $randomClass = $bgClasses[array_rand($bgClasses)];
+    $index = session('widget_color_index', 0);
+    $fixedClass = $bgClasses[$index % count($bgClasses)];
+    $index++;
+    session(['widget_color_index' => $index]);
 @endphp
 
 <div
-    {{ $attributes->merge(['class' => "$randomClass p-20 rounded b-shadow-4 d-flex justify-content-between align-items-center"]) }}>
+    {{ $attributes->merge(['class' => "$fixedClass p-20 rounded b-shadow-4 d-flex justify-content-between align-items-center"]) }}>
     <div class="d-block text-capitalize">
         <h5 class="f-15 f-w-500 mb-20 text-darkest-grey">{{ $title }}
             @if (!is_null($info))
@@ -12,7 +15,7 @@
             @endif
         </h5>
         <div class="d-flex">
-            <p class="mb-0 f-15 font-weight-bold text-indigo d-grid">
+            <p class="mb-0 f-15 font-weight-bold text-white d-grid">
                 <span id="{{ $widgetId }}">{{ $value }}</span>
             </p>
         </div>
